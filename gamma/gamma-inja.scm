7d55b5bf140b6535c336e68d8e8017815035b80f
Julien Vanharen <julien.vanharen@inria.fr>
2025-10-22 19:06:10 +0200

    inja

--- gamma/gamma-inja.scm
+++ gamma/gamma-inja.scm
@@ -11,19 +11,16 @@
 (define-public gamma-inja
   (package
     (name "gamma-inja")
-    (version "3.4.0")
+    (version "3.5.0")
     (source
       (origin
-        (method git-fetch)
-        (uri (git-reference
-              (url "https://github.com/pantor/inja")
-              (commit "b227644")))
-        (file-name (git-file-name name version))
-        (sha256 (base32 "15aiycrhacz8wwz2fpbr4yaxs3sxdxb9xpbskg5nkpwdx93ill87"))))
+        (method url-fetch)
+        (uri "https://github.com/pantor/inja/releases/download/v3.5.0/inja.hpp")
+        (sha256 (base32 "0jrngz72dfqlm079sx4y0khiirpscyq1v3lyjrbg57lswia3lgaz"))))
     (inputs
      (list gamma-nlohmann-json))
     (build-system copy-build-system)
-    (arguments '(#:install-plan '(("single_include/inja/inja.hpp" "include/inja/inja.hpp"))))
+    (arguments '(#:install-plan '(("inja.hpp" "include/inja/inja.hpp"))))
     (home-page "https://github.com/pantor/inja")
     (synopsis "A Template Engine for Modern C++")
     (description "Inja is a template engine for modern C++, loosely inspired by jinja for python. It has an easy and yet powerful template syntax with all variables, loops, conditions, includes, callbacks, and comments you need, nested and combined as you like.")
